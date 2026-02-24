6a8649f59c3e4ac10f339813089456f256563096
keenan.gebze <keenan.gebze@gmail.com>
2026-02-24 21:36:22 +0700

    add dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,16 +5,19 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { NamespaceProvider } from '../context/NamespaceContext';
 import Nav from '../components/Nav';
+import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NamespaceProvider>
-      <div className="min-h-screen flex flex-col">
-        <Nav />
-        <main className="container py-8">
-          <Component {...pageProps} />
-        </main>
-      </div>
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        <div className="min-h-screen flex flex-col">
+          <Nav />
+          <main className="container py-8">
+            <Component {...pageProps} />
+          </main>
+        </div>
+      </ThemeProvider>
     </NamespaceProvider>
   );
 }
